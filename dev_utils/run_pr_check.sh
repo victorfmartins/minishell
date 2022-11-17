ddac08d18fc4df932e7849e92db9292aeeb1efb3
Victor Franco Martins <vaita1997@gmail.com>
2022-11-17 16:26:56 -0300

    add test to checker rule

--- dev_utils/run_pr_check.sh
+++ dev_utils/run_pr_check.sh
@@ -15,3 +15,42 @@ else
 	echo -e "$YELLOW$error$RESET"
 	exit 1
 fi
+
+make
+echo "I'm the infile file" > infile
+
+rm output_minishell output_shell
+>output_minishell; >output_shell; diff output_minishell output_shell
+
+< infile ls -l | wc -l >> output_shell
+< infile cat /etc/passwd | grep -v "#" | wc -l >> output_shell
+< infile cat | grep name | grep -E a$ | wc -l >> output_shell
+< infile cat | grep -i a | grep -E e$ >> output_shell
+< infile cat | grep -v a | grep -E s$ >> output_shell
+< infile cat | grep -i s | sort | head -n 3 | uniq | tail -5 | wc -l >> output_shell
+< infile cat | grep -i s | head -n 3 | sort | uniq | tail -5 >> output_shell
+
+./minishell <<EOF
+< infile ls -l | wc -l >> output_minishell
+< infile cat /etc/passwd | grep -v "#" | wc -l >> output_minishell
+< infile cat | grep name | grep -E a$ | wc -l >> output_minishell
+< infile cat | grep -i a | grep -E e$ >> output_minishell
+< infile cat | grep -v a | grep -E s$ >> output_minishell
+< infile cat | grep -i s | sort | head -n 3 | uniq | tail -5 | wc -l >> output_minishell
+< infile cat | grep -i s | head -n 3 | sort | uniq | tail -5 >> output_minishell
+exit
+EOF
+
+COMPARE=`(diff output_minishell output_shell | wc -c)`
+error=$(diff output_minishell output_shell)
+
+rm output_minishell output_shell infile
+make fclean_all
+
+if [ "$COMPARE" = 0 ] ; then
+	echo -e "$CYAN Test:$RESET$GREEN OK$RESET"
+else
+	echo -e "$CYAN Norminette:$RESET$RED KO$RESET"
+	echo -e "$YELLOW$error$RESET"
+	exit 1
+fi
